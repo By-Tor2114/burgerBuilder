cd3fedebf6404eed8f0125dc477e3d1a70679a1b
By-Tor2114 <by_tor2114_uk@yahoo.com>
2019-12-05 09:42:02 +0000

    Adds a placeholder message for when no ingredients have been added to burger

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -3,11 +3,19 @@ import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredients/BurgerIngredient';
 
 const Burger = ({ ingredients }) => {
-  const transformedIngredients = Object.keys(ingredients).map(igKey => {
-    return [...Array(ingredients[igKey])].map((_, index) => {
-      return <BurgerIngredient key={igKey + index} type={igKey} />;
-    });
-  });
+  let transformedIngredients = Object.keys(ingredients)
+    .map(igKey => {
+      return [...Array(ingredients[igKey])].map((_, index) => {
+        return <BurgerIngredient key={igKey + index} type={igKey} />;
+      });
+    })
+    .reduce((a, b) => {
+      return a.concat(b);
+    }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
 
   return (
     <div className={classes.Burger}>
