4837bcec69b53114e2e8fbf70bbd984c0aa00505
By-Tor2114 <by_tor2114_uk@yahoo.com>
2019-12-05 11:55:26 +0000

    Adds a function to handle adding new fillings to burger. Updates state and initial price accordingly

--- src/components/Burger/BuildControls/BuildControls.jsx
+++ src/components/Burger/BuildControls/BuildControls.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classes from './BuildControls.module.css';
+import BuildControl from './BuildControl/BuildControl';
 
-const BuildControls = props => {
-  return <div></div>;
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' }
+];
+
+const BuildControls = ({ ingredientAdded }) => {
+  return (
+    <div className={classes.BuildControls}>
+      {controls.map(control => {
+        return (
+          <BuildControl
+            added={() => ingredientAdded(control.type)}
+            key={control.label}
+            label={control.label}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default BuildControls;
--- src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -1,5 +1,13 @@
 import React, { Component, Fragment } from 'react';
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+
+const INGREDIENT_PRICES = {
+  salad: 0.5,
+  cheese: 0.4,
+  meat: 1.4,
+  bacon: 0.7
+};
 
 class BurgerBuilder extends Component {
   state = {
@@ -8,15 +16,32 @@ class BurgerBuilder extends Component {
       bacon: 1,
       cheese: 2,
       meat: 2
-    }
+    },
+    totalPrice: 4
   };
+
+  addIngredient = type => {
+    const priceAddition = INGREDIENT_PRICES[type];
+
+    this.setState(currentState => {
+      return {
+        totalPrice: currentState.totalPrice + priceAddition,
+        ingredients: {
+          ...currentState.ingredients,
+          [type]: currentState.ingredients[type] + 1
+        }
+      };
+    });
+  };
+
+  removeIngredient = type => {};
 
   render() {
     const { ingredients } = this.state;
     return (
       <Fragment>
         <Burger ingredients={ingredients} />
-        <div>Build Controls</div>
+        <BuildControls ingredientAdded={this.addIngredient} />
       </Fragment>
     );
   }
