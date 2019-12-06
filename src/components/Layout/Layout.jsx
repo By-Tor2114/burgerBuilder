d071d0b24c5aebda9c07188a85d2d802398b490c
By-Tor2114 <by_tor2114_uk@yahoo.com>
2019-12-06 11:55:32 +0000

    Adds ability to hide sidedrawer

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,27 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = props => {
-  return (
-    <Fragment>
-      <Toolbar /> <main className={classes.Content}>{props.children}</main>
-      <SideDrawer />
-    </Fragment>
-  );
-};
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  };
+
+  sideDrawerClose = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  render() {
+    const { showSideDrawer } = this.state;
+    return (
+      <Fragment>
+        <Toolbar />
+        <main className={classes.Content}>{this.props.children}</main>
+        <SideDrawer open={showSideDrawer} closed={this.sideDrawerClose} />
+      </Fragment>
+    );
+  }
+}
 
 export default Layout;
