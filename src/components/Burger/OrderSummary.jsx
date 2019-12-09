296796e414113326e218fd1924574c266c10fc44
By-Tor2114 <by_tor2114_uk@yahoo.com>
2019-12-09 07:15:37 +0000

    Adds shouldComponentUpdate check to Modal

--- src/components/Burger/OrderSummary.jsx
+++ src/components/Burger/OrderSummary.jsx
@@ -1,39 +1,42 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 import Button from '../UI/Button/Button';
 
-const OrderSummary = ({
-  ingredients,
-  purchaseCancel,
-  purchaseContinue,
-  price
-}) => {
-  const ingredientSummary = Object.keys(ingredients).map(igKey => {
-    return (
-      <li key={igKey}>
-        <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
-        {ingredients[igKey]}
-      </li>
-    );
-  });
+class OrderSummary extends Component {
+  componentDidUpdate() {
+    console.log('order summary will update');
+  }
 
-  return (
-    <Fragment>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        {' '}
-        <strong> Total Price: £{price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button clicked={purchaseCancel} buttonType={'Danger'}>
-        CANCEL
-      </Button>
-      <Button clicked={purchaseContinue} buttonType={'Success'}>
-        CONTINUE
-      </Button>
-    </Fragment>
-  );
-};
+  render() {
+    const { ingredients, price, purchaseCancel, purchaseContinue } = this.props;
+
+    const ingredientSummary = Object.keys(ingredients).map(igKey => {
+      return (
+        <li key={igKey}>
+          <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
+          {ingredients[igKey]}
+        </li>
+      );
+    });
+
+    return (
+      <Fragment>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          {' '}
+          <strong> Total Price: £{price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button clicked={purchaseCancel} buttonType={'Danger'}>
+          CANCEL
+        </Button>
+        <Button clicked={purchaseContinue} buttonType={'Success'}>
+          CONTINUE
+        </Button>
+      </Fragment>
+    );
+  }
+}
 
 export default OrderSummary;
