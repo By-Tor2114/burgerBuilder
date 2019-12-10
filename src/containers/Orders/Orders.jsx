7be6d097b6c544100e9a8a54724cff15227841a8
By-Tor2114 <by_tor2114_uk@yahoo.com>
2019-12-10 10:20:52 +0000

    Adds GET request to orders component for fetching orders from firebase

--- src/containers/Orders/Orders.jsx
+++ src/containers/Orders/Orders.jsx
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends Component {
+  state = {
+    orders: [],
+    isLoading: true
+  };
+  componentDidMount = async () => {
+    try {
+      const { data } = await axios.get('/orders.json');
+      const orders = [];
+
+      for (let key in data) {
+        orders.push({ ...data[key], id: key });
+      }
+
+      this.setState({ orders, isLoading: false });
+    } catch (error) {
+      this.setState({ isLoading: false });
+    }
+  };
+
   render() {
     return (
       <div>
@@ -12,4 +33,4 @@ class Orders extends Component {
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
