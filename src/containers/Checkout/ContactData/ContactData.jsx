91a763c1bea87f41c5306f16c0927a41a7eeaed7
By-Tor2114 <by_tor2114_uk@yahoo.com>
2019-12-11 08:02:05 +0000

    Amends error with default value on delivery method

--- src/containers/Checkout/ContactData/ContactData.jsx
+++ src/containers/Checkout/ContactData/ContactData.jsx
@@ -71,7 +71,7 @@ class ContactData extends Component {
             { value: 'cheapest', displayValue: 'Cheapest' }
           ]
         },
-        value: '',
+        value: 'fastest',
         valid: true,
         validation: {}
       }
