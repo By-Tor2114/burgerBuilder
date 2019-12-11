30987b6e29313f0b6ab32c634287e2e4f1ed5b26
By-Tor2114 <by_tor2114_uk@yahoo.com>
2019-12-11 10:44:21 +0000

    Adds redux setup to index.js

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,10 +1,10 @@
-import * actionTypes from './actions'
+import * as actionTypes from './actions';
 
 const initialState = {
   ingredients: null,
-  totalPrice: 4,
-}
+  totalPrice: 4
+};
 
-const reducer = (state=initialState, action) => {
+const reducer = (state = initialState, action) => {};
 
-}
+export default reducer;
