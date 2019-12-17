825c103ff87101526bdeebb611f8ca1ae5607dcb
By-Tor2114 <by_tor2114_uk@yahoo.com>
2019-12-17 10:39:01 +0000

    Adds email regex check on contact data component

--- src/containers/Checkout/ContactData/ContactData.jsx
+++ src/containers/Checkout/ContactData/ContactData.jsx
@@ -63,7 +63,7 @@ class ContactData extends Component {
           placeholder: 'Email'
         },
         value: '',
-        validation: { required: true },
+        validation: { required: true, isEmail: true },
         valid: false,
         touched: false
       },
@@ -114,6 +114,11 @@ class ContactData extends Component {
 
     if (rules.maxLength) {
       isValid = value.length <= rules.maxLength && isValid;
+    }
+
+    if (rules.isEmail) {
+      const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+      isValid = pattern.test(value) && isValid;
     }
 
     return isValid;
