150bed7d91ad38650aaccb248f2c805d808f8b44
By-Tor2114 <by_tor2114_uk@yahoo.com>
2019-12-17 09:22:39 +0000

    Amends mobile view for orders screen

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -13,16 +13,7 @@ const Order = props => {
 
   const ingredientOutput = ingredients.map(ing => {
     return (
-      <span
-        key={ing.name}
-        style={{
-          textTransform: 'capitalize',
-          display: 'inline-block',
-          margin: '0 8px',
-          border: '1px solid #ccc',
-          padding: '5px'
-        }}
-      >
+      <span key={ing.name} className={classes.Ingredients}>
         {ing.name} ({ing.amount})
       </span>
     );
@@ -30,8 +21,8 @@ const Order = props => {
 
   return (
     <div className={classes.Order}>
-      <p>Ingredients: {ingredientOutput}</p>
-      <p>
+      <p className={classes.Ingredients}>Ingredients:{ingredientOutput}</p>
+      <p style={{ marginLeft: '7px' }}>
         Price: <strong>£{props.price.toFixed(2)}</strong>
       </p>
     </div>
